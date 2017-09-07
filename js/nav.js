aacd81e42363f8b9f83931c89c027aef71c25deb
AlittleFenQing <1443217645@qq.com>
2017-09-07 21:06:34 +0800

    left-menu

---
+++ js/nav.js
@@ -0,0 +1,54 @@
+var can = document.getElementById('myCanvas');
+//创建一个画布
+var ctx = can.getContext('2d');
+var draw = function(x1, y1, x2, y2, x3, y3, color, type) {
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.lineTo(x3, y3);
+    ctx[type + 'Style'] = color;
+    ctx.closePath();
+    ctx[type]();
+}
+draw(0,0,120,0,0,120,'rgba(255,255,255,0.3)','fill');
+
+
+
+
+
+
+
+(function() {
+			[].slice.call(document.querySelectorAll('.menu')).forEach(function(menu) {
+				var menuItems = menu.querySelectorAll('.menu__link'),
+					setCurrent = function(ev) {
+						ev.preventDefault();
+
+						var item = ev.target.parentNode; // li
+
+						// return if already current
+						if (classie.has(item, 'menu__item--current')) {
+							return false;
+						}
+						// remove current
+						classie.remove(menu.querySelector('.menu__item--current'), 'menu__item--current');
+						// set current
+						classie.add(item, 'menu__item--current');
+					};
+
+				[].slice.call(menuItems).forEach(function(el) {
+					el.addEventListener('click', setCurrent);
+				});
+			});
+
+			[].slice.call(document.querySelectorAll('.link-copy')).forEach(function(link) {
+				link.setAttribute('data-clipboard-text', location.protocol + '//' + location.host + location.pathname + '#' + link.parentNode.id);
+				new Clipboard(link);
+				link.addEventListener('click', function() {
+					classie.add(link, 'link-copy--animate');
+					setTimeout(function() {
+						classie.remove(link, 'link-copy--animate');
+					}, 300);
+				});
+			});
+		})(window);
